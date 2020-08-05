976e2723d6e742058dc66f7b6686f4450d2b69c1
Anderson Torres <atorresnh@gmail.com>
2020-08-05 17:48:16 -0300

    Yes !

--- hello.php
+++ hello.php
@@ -4,7 +4,7 @@
  * @version 1.6
  */
 /*
-Plugin Name: Hello Dolly
+Plugin Name: Hello Bob Marley
 Plugin URI: http://wordpress.org/extend/plugins/hello-dolly/
 Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
 Author: Matt Mullenweg
@@ -14,34 +14,32 @@ Author URI: http://ma.tt/
 
 function hello_dolly_get_lyric() {
 	/** These are the lyrics to Hello Dolly */
-	$lyrics = "Hello, Dolly
-Well, hello, Dolly
-It's so nice to have you back where you belong
-You're lookin' swell, Dolly
-I can tell, Dolly
-You're still glowin', you're still crowin'
-You're still goin' strong
-We feel the room swayin'
-While the band's playin'
-One of your old favourite songs from way back when
-So, take her wrap, fellas
-Find her an empty lap, fellas
-Dolly'll never go away again
-Hello, Dolly
-Well, hello, Dolly
-It's so nice to have you back where you belong
-You're lookin' swell, Dolly
-I can tell, Dolly
-You're still glowin', you're still crowin'
-You're still goin' strong
-We feel the room swayin'
-While the band's playin'
-One of your old favourite songs from way back when
-Golly, gee, fellas
-Find her a vacant knee, fellas
-Dolly'll never go away
-Dolly'll never go away
-Dolly'll never go away again";
+	$lyrics = "Ooh, yeah
+	Well, alright
+	We're jammin'
+	Sing it, I want to jam it with you
+	We're jammin', we're jammin'
+	And I hope you like jammin' too
+	Ain't no rules, ain't no vow
+	You can do it anyhow
+	'Cause I and I will see you through
+	But every day we pay the price
+	Come a little sacrifice
+	Jammin' 'til the jam is through
+	Ooh yeah, we're jammin', hey
+	To think that jammin' was a thing of the past
+	We're jammin', we're jammin'
+	And I hope this jam is gonna last
+	No bullet can stop us now, we neither beg nor we won't bow
+	Neither can be bought nor sold
+	We all defend the right, Jah-Jah children must unite
+	'Cause life is worth much more than gold
+	Ooh yeah, we're jammin'
+	And we're jammin' in the name of the Lord
+	We're jammin', we're jammin'
+	And we're jammin' right straight from yard
+	Ay
+	Holy…";
 
 	// Here we split it into lines
 	$lyrics = explode( "\n", $lyrics );
